3e9229159e0f146f4a53247ecb522d223b40ce08
jj <tangjun@zuzuche.com>
2024-12-27 15:52:27 +0800

    fix

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,12 +2,11 @@ const Login = ({ credential, setCredential, onLogin }) => {
   return (
     <div className="auth-container">
       <h1>迷你仓</h1>
-      <p>输入任意凭证即可开始使用</p>
       <input
         type="text"
         value={credential}
         onChange={(e) => setCredential(e.target.value)}
-        placeholder="输入凭证"
+        placeholder="输入任意凭证即可开始使用"
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
             onLogin()
