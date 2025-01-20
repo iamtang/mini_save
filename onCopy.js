55c85486a05b7f7120f463ad2b7ae9e759bee688
jj <tangjun@zuzuche.com>
2025-01-20 17:21:06 +0800

    add

--- onCopy.js
+++ onCopy.js
@@ -1,33 +1,34 @@
-const { clipboard } =  require('electron');
+const { clipboard, app } =  require('electron');
 const axios =  require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
+const path = require('path');
 const WebSocket = require('ws');
 let preContent = null;
 let socket = null
 
+
 function onCopy(server, {url, credential, isServer}){
     if(!credential) return null
-    socket = isServer ? initServerWss(server) : initClientWss(url)
+    socket = isServer ? initServerWss(server) : initClientWss({url, credential})
     let currentContent = clipboard.read('public.file-url') || clipboard.readText();
     setInterval(async () => {
       currentContent = clipboard.read('public.file-url') || clipboard.readText();
-      if(currentContent === preContent) return null;
+      if(currentContent === preContent || preContent && preContent.includes(app.getPath('userData'))) return null;
       if(clipboard.read('public.file-url')){
         const form = new FormData();
         form.append('file', fs.createReadStream(decodeURIComponent(currentContent.replace('file://',''))));
         const res = await axios.post(`http://${url}api/upload/${credential}`, form, {headers: form.getHeaders() })
         socket.send(JSON.stringify({type: 'file', data: res.data.id}))
-        console.log('发送==1')
+        console.log('===文件===')
         preContent = currentContent
       }else{
         let currentContent = clipboard.readText();
         await axios.post(`http://${url}api/text/${credential}`, {text: currentContent})
         socket.send(JSON.stringify({type: 'text', data: currentContent}))
-        console.log('发送==2')
+        console.log('===文本===')
         preContent = currentContent
       }
-      
     }, 1000);
 }
 
@@ -63,14 +64,73 @@ function initServerWss(server){
     }}
 }
 
-function reConnentClientWss(url){客户端连接成功
+function clearFolderContents(folderPath) {
+    // 获取文件夹中的所有内容
+    const files = fs.readdirSync(folderPath);
+  
+    // 遍历每个文件/子文件夹
+    files.forEach(file => {
+        const currentPath = path.join(folderPath, file);
+    
+        // 获取文件/目录的状态
+        const stats = fs.statSync(currentPath);
+    
+        if (stats.isDirectory()) {
+            // 如果是目录，递归删除其中的文件
+            clearFolderContents(currentPath);
+    
+            // 删除空目录
+            fs.rmdirSync(currentPath);
+        } else {
+            // 如果是文件，删除文件
+            fs.unlinkSync(currentPath);
+        }
+    });
+}
+
+async function downloadFile(url){
+    const savePath = path.join(app.getPath('userData'), 'downloads');
+    // fs.rmdirSync(savePath);
+    clearFolderContents(savePath)
+    const response = await axios({method: 'get', url, responseType: 'arraybuffer'})
+    const disposition = response.headers['content-disposition'];
+    let fileName = 'default-file';  // 默认文件名
+
+    // 解析 Content-Disposition 获取文件名
+    if (disposition && disposition.indexOf('attachment') !== -1) {
+      try {
+        // 尝试解析文件名，并解码
+        const fileNameMatch = disposition.match(/filename\*?=UTF-8''(.+)/);
+        if (fileNameMatch && fileNameMatch[1]) {
+          // 解码文件名，URL 解码
+          fileName = decodeURIComponent(fileNameMatch[1]);
+        }
+      } catch (err) {
+        console.error('解码文件名时发生错误:', err);
+        fileName = 'default-file';  // 出现错误时使用默认文件名
+      }
+    }
+
+    // 文件保存路径
+    const saveFilePath = path.join(savePath, fileName);
+
+    // 确保目标文件夹存在
+    const downloadDir = path.dirname(saveFilePath);
+    if (!fs.existsSync(downloadDir)) {
+      fs.mkdirSync(downloadDir, { recursive: true });
+    }
+    fs.writeFileSync(saveFilePath, Buffer.from(response.data))
+    return saveFilePath
+}
+
+function reConnentClientWss(url){
     setTimeout(() => {
         console.log('正在重连')
         socket = initClientWss(url);
     }, 5000)
 }
 
-function initClientWss(url){
+function initClientWss({url, credential}){
     const socket = new WebSocket(`ws://${url}`);
     socket.on('open', () => {
         console.log('WebSocket 连接成功');
@@ -79,12 +139,16 @@ function initClientWss(url){
             if(data.type === 'text'){
                 preContent = data.data
                 clipboard.writeText(data.data)
+            }else if(data.type === 'file'){
+                downloadFile(`http://${url}api/download/${credential}/${data.data}`).then(res => {
+                    preContent = res
+                    clipboard.writeBuffer('public.file-url', Buffer.from(`file://${res}`, 'utf-8'));
+
+                })
             }
-            
             console.log(`收到消息: ${msg}`);
         });
         setInterval(() => {
-        console.warn('WebSocket 连接关闭');
             socket.send(JSON.stringify({type: 'ping'}));
         }, 30000);
     });
