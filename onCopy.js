1e3ee1d52def1d0c5b5c3c0a05235f91bf9c8c94
jj <tangjun@zuzuche.com>
2025-01-22 16:53:39 +0800

    add

--- onCopy.js
+++ onCopy.js
@@ -12,6 +12,8 @@ log.initialize()
 let preContent = null;
 let currentContent = null;
 let socket = null
+// 房间管理对象
+const rooms = new Map(); // 使用 Map 存储房间和客户端连接
 
 function onCopy(server, {isServer, url, CREDENTIAL, MAX_FILE_SIZE = 50}){
     if(!CREDENTIAL) return null
@@ -41,8 +43,10 @@ function onCopy(server, {isServer, url, CREDENTIAL, MAX_FILE_SIZE = 50}){
     }, 1000);
 }
 
-function onMessage(msg, { url, CREDENTIAL }){
+function onMessage(msg, { url, CREDENTIAL, roomID }){
     const data = JSON.parse(msg);
+    // 口令不一致，无需同步
+    if(roomID !== CREDENTIAL) return;
     // data.type !== 'ping' && log.info(data, '=======')
     if (data.type === 'text') {
         currentContent = preContent = data.data
@@ -60,9 +64,25 @@ function initServerWss(server, { url, CREDENTIAL }) {
 	// 处理 WebSocket 连接
 	wss.on('connection', (ws, req) => {
         const roomID = req.url.slice(1); // 获取路径部分去掉 "/"
+        if (!rooms.has(roomID)) {
+            rooms.set(roomID, new Set()); // 如果房间不存在，创建房间
+        }
+        // 将客户端加入房间
+        rooms.get(roomID).add(ws);
 		log.info('客户端连接成功', roomID);
 		// 监听客户端发送的消息
-		ws.on('message', (msg) => onMessage(msg, { url, CREDENTIAL }));
+		ws.on('message', (msg) => {
+            onMessage(msg, { url, CREDENTIAL, roomID })
+            // 同步给同口令的设备
+            for (const client of rooms.get(roomID)) {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(msg);
+                }
+            }
+        });
+        ws.on('close', () => {
+            rooms.get(roomID).delete(ws);
+        })
 	});
 
 	// 在 HTTP 服务器上升级到 WebSocket
@@ -73,11 +93,13 @@ function initServerWss(server, { url, CREDENTIAL }) {
 	});
 	return {
 		send(data) {
-			wss.clients.forEach((client) => {
-				if (client.readyState === WebSocket.OPEN) {
-					client.send(data)
-				}
-			})
+            if(!rooms.get(CREDENTIAL)) return
+            // 同步给同口令的设备
+            for (const client of rooms.get(CREDENTIAL)) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            }
 		}
 	}
 }
