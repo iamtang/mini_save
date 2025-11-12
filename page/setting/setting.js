7a6cebb6c15447f3c249d40dc0b61a78763a22d4
jj <tangjun@zuzuche.com>
2025-11-12 15:27:06 +0800

    add

--- page/setting/setting.js
+++ page/setting/setting.js
@@ -9,7 +9,7 @@ function createSettingWindow() {
   
   settingWindow = new BrowserWindow({
     width: 360,
-    height: 340,
+    height: 300,
     resizable: false,
     devTools: false, // 禁用开发者工具
     // transparent: true,
